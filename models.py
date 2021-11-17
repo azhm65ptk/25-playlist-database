95168bca9b7cfbefa950fa0f09bb494a8c092eaf
Aung Myint <Austin@AungZinyintsMBP.lan>
2021-11-17 13:35:26 -0500

    first commit

---
+++ models.py
@@ -0,0 +1,48 @@
+"""Models for Playlist app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Playlist(db.Model):
+    """Playlist."""
+
+    # ADD THE NECESSARY CODE HERE
+    __tablename__="playlists"
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name=db.Column(db.String(20), unique= True, nullable=False)
+    description=db.Column(db.String(50),nullable=False)
+    songs=db.relationship('Song',secondary='playlist_songs', backref='playlists')
+    
+
+
+
+class Song(db.Model):
+    """Song."""
+
+    # ADD THE NECESSARY CODE HERE
+    __tablename__='songs'
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title=db.Column(db.String(30),nullable=False)
+    artist=db.Column(db.String(50),nullable=False)
+   
+
+
+class PlaylistSong(db.Model):
+    """Mapping of a playlist to a song."""
+
+    # ADD THE NECESSARY CODE HERE
+    __tablename__='playlist_songs'
+    id=db.Column(db.Integer, primary_key=True, nullable=False)
+    
+    playlist_id=db.Column(db.Integer, db.ForeignKey('playlists.id'),nullable=False)
+    song_id=db.Column(db.Integer,db.ForeignKey('songs.id'),nullable=False)
+
+
+# DO NOT MODIFY THIS FUNCTION
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
